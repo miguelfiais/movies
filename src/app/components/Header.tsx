de8a44ece53b175417e212111fa8cadbcb8922c4
miguelfiais <miguel.fiais10@gmail.com>
2023-08-12 18:13:10 -0300

    feat: add main movie on home screen

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,21 +2,23 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <header className="container mx-auto flex items-center justify-between p-5">
-      <Image src="/logo.png" alt="logo" width={300} height={54.69} />
-      <nav>
-        <ul className="flex items-center gap-8 text-white font-medium">
-          <li>
-            <a href="">Filmes</a>
-          </li>
-          <li>
-            <a href="">Series</a>
-          </li>
-          <li>
-            <a href="">Outros</a>
-          </li>
-        </ul>
-      </nav>
+    <header className="p-5 fixed z-10 w-full">
+      <div className="container mx-auto flex items-center justify-between">
+        <Image src="/logo.png" alt="logo" width={300} height={54.69} />
+        <nav>
+          <ul className="flex items-center gap-8 text-white font-medium">
+            <li>
+              <a href="">Filmes</a>
+            </li>
+            <li>
+              <a href="">Series</a>
+            </li>
+            <li>
+              <a href="">Outros</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,64 @@
-export default function Home() {
-  return <div>Home</div>
+import { api } from '@/services/api'
+import Image from 'next/image'
+
+interface IMovie {
+  data: {
+    results: {
+      title: string
+      overview: string
+      poster_path: string
+      backdrop_path: string
+    }[]
+  }
+}
+
+const fetchMovies = async () => {
+  try {
+    const {
+      data: { results },
+    } = (await api.get('/movie/popular')) as IMovie
+    return results
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function Home() {
+  const movies = await fetchMovies()
+
+  if (!movies) return null
+
+  const numberRandom = Math.floor(Math.random() * (20 - 0))
+  const movieRandom = movies[numberRandom]
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${movieRandom.backdrop_path})`,
+      }}
+      className={`relative h-screen bg-no-repeat bg-cover bg-center flex items-center justify-center before:absolute before:w-full before:h-full before:bg-black before:bg-opacity-60`}
+    >
+      <div className="container  mx-auto z-10 text-white flex items-center justify-evenly">
+        <div className="max-w-xl">
+          <h1 className="text-4xl font-bold">{movieRandom.title}</h1>
+          <p className="text-lg my-5">{movieRandom.overview}</p>
+          <div className="flex items-center gap-5">
+            <button className="py-2 px-4 bg-red-600 font-medium text-white rounded-3xl shadow-md shadow-red-600 border-2 border-red-600">
+              Assista agora
+            </button>
+            <button className="py-2 px-4 border-2 rounded-3xl font-medium hover:bg-white hover:text-red-600">
+              Assista o trailer
+            </button>
+          </div>
+        </div>
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movieRandom.poster_path}`}
+          alt={movieRandom.title}
+          width={300}
+          height={450}
+          className="rounded-xl"
+        />
+      </div>
+    </div>
+  )
 }
